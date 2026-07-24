4826470d5d5cedd5001c5a42da4f21dc624c3c27
Shivsai Jagadale <shivsai1396@gmail.com>
2026-07-24 19:20:57 +0530

    06_Exception_Handling

---
+++ 01_Try_except.py
@@ -0,0 +1,14 @@
+try:
+    a=int(input("Enter a number: "))
+    print(a)
+
+except ValueError as v:
+    print("Enter a valid number") 
+    a=int(input("Enter a number: "))
+
+except Exception as e:
+    print(f"The number is {a}")
+
+
+print(f"The number is {a}")
+print("Thank You")           
